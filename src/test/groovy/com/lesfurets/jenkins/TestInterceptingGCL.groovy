brianeray/JenkinsPipelineUnit

commit 6f2639f1805fa60860d7e21e124fa58f2a9375d5

Allow library classes access env

src/main/groovy/com/lesfurets/jenkins/unit/global/lib/LibraryLoader.groovy
@@ -110,13 +110,13 @@ class LibraryLoader {
                     // prevent fd leak on the DirectoryStream from Files.list()
                     ds.close()
                 }
-                // pre-load library classes using 
+                // pre-load library classes using JPU groovy class loader
                 if (srcPath.toFile().exists()) {
-                    println "tracing files in $srcPath"
                     srcPath.toFile().eachFileRecurse (FILES) { File srcFile ->
-                        println "parsing file $srcFile"
-                        Class clazz = groovyClassLoader.parseClass(srcFile)
-                        groovyClassLoader.loadClass(clazz.name)
+                        if (srcFile.name.endsWith(".groovy")) {
+                            Class clazz = groovyClassLoader.parseClass(srcFile)
+                            groovyClassLoader.loadClass(clazz.name)
+                        }
                     }
                 }
             }

src/test/groovy/com/lesfurets/jenkins/TestInterceptingGCL.groovy
@@ -15,6 +15,7 @@ class TestInterceptingGCL extends BasePipelineTest {
     void setUp() throws Exception {
         scriptRoots += 'src/test/jenkins'
         super.setUp()
+        addEnvVar("FOO","bar")
     }
     String sharedLibVars = this.class
         .getResource('/libs/test_cross_vars_usage')

src/main/groovy/com/lesfurets/jenkins/unit/BasePipelineTest.groovy
@@ -62,6 +62,7 @@ abstract class BasePipelineTest {
             it.baseClassloader = this.baseClassLoader
             it.imports += this.imports
             it.baseScriptRoot = this.baseScriptRoot
+            it.binding = this.binding
             return it
         }.init()
     }
@@ -246,7 +247,7 @@ abstract class BasePipelineTest {
      * @return the return value of the script
      */
     Object runScript(Script script) {
-        return helper.runScript(script)
+        return helper.runScript(script, this.binding)
     }
 
     @Memoized

src/main/groovy/com/lesfurets/jenkins/unit/InterceptingGCL.groovy
@@ -7,11 +7,11 @@ import static com.lesfurets.jenkins.unit.MethodSignature.method
 
 class InterceptingGCL extends GroovyClassLoader {
 
-    static void interceptClassMethods(MetaClass metaClazz, PipelineTestHelper helper) {
+    static void interceptClassMethods(MetaClass metaClazz, PipelineTestHelper helper, Binding binding) {
         metaClazz.invokeMethod = helper.getMethodInterceptor()
         metaClazz.static.invokeMethod = helper.getMethodInterceptor()
         metaClazz.methodMissing = helper.getMethodMissingInterceptor()
-
+        metaClazz.getEnv = {return binding.env}
         // find and replace script method closure with any matching allowed method closure
         metaClazz.methods.forEach { scriptMethod ->
             def signature = method(scriptMethod.name, scriptMethod.nativeParameterTypes)
@@ -24,20 +24,22 @@ class InterceptingGCL extends GroovyClassLoader {
     }
 
     PipelineTestHelper helper
+    Binding binding
 
     InterceptingGCL(PipelineTestHelper helper,
                     ClassLoader loader,
-                    CompilerConfiguration config) {
+                    CompilerConfiguration config,
+                    Binding binding) {
         super(loader, config)
         this.helper = helper
+        this.binding = binding
     }
 
     @Override
     Class parseClass(GroovyCodeSource codeSource, boolean shouldCacheSource)
             throws CompilationFailedException {
-        println "Parsing class: ${codeSource.getName()}"
         Class clazz = super.parseClass(codeSource, shouldCacheSource)
-        interceptClassMethods(clazz.metaClass, helper)
+        interceptClassMethods(clazz.metaClass, helper, binding)
         return clazz
     }
 
@@ -62,7 +64,7 @@ class InterceptingGCL extends GroovyClassLoader {
 
         if (cls == null) {
             // no class found, using parent's method
-            cls = super.loadClass(name)
+            return super.loadClass(name)
         }
 
         // Copy from this.parseClass(GroovyCodeSource, boolean)

src/main/groovy/com/lesfurets/jenkins/unit/PipelineTestHelper.groovy
@@ -66,10 +66,10 @@ class PipelineTestHelper {
     List<MethodCall> callStack = []
 
     /**
-     * Controls whether method call arguments are cloned when pushed onto the 
-     * call stack. Disabling cloning can be useful for arguments of types that 
-     * are not {@code Cloneable}. Beware, however, arguments that mutate 
-     * during a test may appear with incorrect values in the call stack if 
+     * Controls whether method call arguments are cloned when pushed onto the
+     * call stack. Disabling cloning can be useful for arguments of types that
+     * are not {@code Cloneable}. Beware, however, arguments that mutate
+     * during a test may appear with incorrect values in the call stack if
      * you disable cloning.
      */
     Boolean cloneArgsOnMethodCallRegistration = true
@@ -84,6 +84,8 @@ class PipelineTestHelper {
      */
     protected LibraryLoader libLoader
 
+    /** Let scripts and library classes access global vars (env, currentBuild) */
+    protected Binding binding
 
     /**
     * Get library loader object
@@ -257,7 +259,7 @@ class PipelineTestHelper {
 
     PipelineTestHelper init() {
         CompilerConfiguration configuration = new CompilerConfiguration()
-        GroovyClassLoader cLoader = new InterceptingGCL(this, baseClassloader, configuration)
+        GroovyClassLoader cLoader = new InterceptingGCL(this, baseClassloader, configuration, binding)
 
         libLoader = new LibraryLoader(cLoader, libraries)
         LibraryAnnotationTransformer libraryTransformer = new LibraryAnnotationTransformer(libLoader)
@@ -371,7 +373,7 @@ class PipelineTestHelper {
         Class scriptClass = gse.loadScriptByName(scriptName)
         setGlobalVars(binding)
         Script script = InvokerHelper.createScript(scriptClass, binding)
-        InterceptingGCL.interceptClassMethods(script.metaClass, this)
+        InterceptingGCL.interceptClassMethods(script.metaClass, this, binding)
         return script
     }
 

src/test/groovy/com/lesfurets/jenkins/TestSharedLibraryWithLocalSourceRetriever.groovy
@@ -67,12 +67,6 @@ class TestSharedLibrary extends BasePipelineTest {
                         .retriever(localSource(sharedLibs))
                         .build()
         helper.registerSharedLibrary(library)
-
-        helper.registerAllowedMethod("library", [String.class], {String expression ->
-            helper.getLibLoader().loadLibrary(expression)
-            return new LibClassLoader(helper,null)
-        })
-
         try {
             def script = loadScript("job/library/${script}.jenkins")
             script.execute()

src/test/resources/libs/test_cross_class_usage/src/org/test/ClassA.groovy
@@ -13,8 +13,10 @@ class ClassA implements Serializable {
     def methodA() {
         if (script) {
             script.sh("echo 'ClassA: $fieldA'")
+            script.echo "env = $script.env"
         } else {
             sh("echo 'ClassA: $fieldA'")
+            echo "env = $env"
         }
     }
 }

src/test/resources/libs/test_cross_class_usage/src/org/test/ClassAB.groovy
@@ -23,8 +23,10 @@ class ClassAB implements Serializable {
     def ownMethod() {
         if(script) {
             script.sh("echo 'ClassAB: $fieldAB'")
+            script.echo "env = $script.env"
         } else {
             sh("echo 'ClassAB: $fieldAB'")
+            echo "env = $env"
         }
     }
 

commit fd1d759e2fbffcec9969fec527699402a8121d15

Issue #224 - Load lib classes before lib vars
Preload lib classes prior to lib vars so that vars can use lib classes
as method argument types without spurious type mismatches and
`MissingMethodException`s. This was an odd side effect of the
class preload logic in 1.4's 5c90975 and 6f2639f, which caused the
classes to be loaded twice, confusing the method interceptor's
matching logic.

src/main/groovy/com/lesfurets/jenkins/unit/global/lib/LibraryLoader.groovy
@@ -95,6 +95,15 @@ class LibraryLoader {
                 groovyClassLoader.addURL(varsPath.toUri().toURL())
                 groovyClassLoader.addURL(resourcesPath.toUri().toURL())
 
+                // pre-load library classes using JPU groovy class loader
+                if (srcPath.toFile().exists()) {
+                    srcPath.toFile().eachFileRecurse (FILES) { File srcFile ->
+                        if (srcFile.name.endsWith(".groovy")) {
+                            Class clazz = groovyClassLoader.parseClass(srcFile)
+                            groovyClassLoader.loadClass(clazz.name)
+                        }
+                    }
+                }
                 if (varsPath.toFile().exists()) {
                     def ds = Files.list(varsPath)
                     ds.map { it.toFile() }
@@ -109,15 +118,6 @@ class LibraryLoader {
                       } as Consumer<String>)
                     // prevent fd leak on the DirectoryStream from Files.list()
                     ds.close()
-                }
-                // pre-load library classes using JPU groovy class loader
-                if (srcPath.toFile().exists()) {
-                    srcPath.toFile().eachFileRecurse (FILES) { File srcFile ->
-                        if (srcFile.name.endsWith(".groovy")) {
-                            Class clazz = groovyClassLoader.parseClass(srcFile)
-                            groovyClassLoader.loadClass(clazz.name)
-                        }
-                    }
                 }
             }
             record.definedGlobalVars = globalVars

src/test/groovy/com/lesfurets/jenkins/TestInterceptingGCL.groovy
@@ -378,5 +378,31 @@ class TestInterceptingGCL extends BasePipelineTest {
         assertCallStack().contains("""test_annotation.sh(echo 'ClassB: I'm field of B')""")
     }
 
+    /**
+    * 1. Load library by annotation, with implicity
+    * 2. Create instance of library class
+    * 3. Call a vars step that accepts that type as an argument, passing that instance
+    * 4. Make sure interception of pipeline methods works propertly
+    */
+    @Test
+    void test_cross_class_as_var_arg_implicit_annotation() throws Exception {
+        //This does not factor in the current test but does replicate the use 
+        //case in which the lazy load feature originated.
+        helper.cloneArgsOnMethodCallRegistration = false
 
+        final library = library().name("test_cross_class_usage")
+                        .defaultVersion("master")
+                        .allowOverride(false)
+                        .implicit(true)
+                        .targetPath(sharedLibCls)
+                        .retriever(projectSource(sharedLibCls))
+                        .build()
+        helper.registerSharedLibrary(library)
+
+        final pipeline = "test_var_with_lib_class_arg_annotation"
+        runScript("job/library/cross_class_pre_loaded/${pipeline}.jenkins")
+        printCallStack()
+        assertCallStack().contains("""${pipeline}.monster1(org.test.Monster1""")
+        assertCallStack().contains("""monster1.echo(Dracula makes quite a scary monster)""")
+    }
 }

src/test/jenkins/job/library/cross_class_pre_loaded/test_var_with_lib_class_arg_annotation.jenkins
@@ -1,0 +1,6 @@
+//Commenting the annotation appears to make no difference when load is implicit
+@Library("test_cross_class_usage")
+import org.test.Monster1
+
+vampire = new Monster1("Dracula")
+monster1(vampire)

src/test/resources/libs/test_cross_class_usage/src/org/test/Monster1.groovy
@@ -1,0 +1,9 @@
+package org.test
+
+class Monster1 {
+    String moniker
+
+    Monster1(String m) {
+      moniker = m
+    }
+}

src/test/resources/libs/test_cross_class_usage/vars/monster1.groovy
@@ -1,0 +1,5 @@
+import org.test.Monster1
+
+void call(Monster1 m1) {
+    echo "$m1.moniker makes quite a scary monster"
+}